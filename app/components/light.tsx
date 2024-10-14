dad4b2819e63e5b41ff6e489cdf6f99e833f6bad
Mateusz <mateusz@adhese.eu>
2024-10-14 14:10:26 +0200

    Progress

--- app/components/light.tsx
+++ app/components/light.tsx
@@ -3,15 +3,45 @@
 import TrafficLight from '../domain/traffic-light'
 import LightConfig, { LightSettings } from '../domain/light-config'
 import Input from './input'
-import { IconButton, Card, CardActions, CardContent, Stack, Grid, Dialog, DialogTitle, DialogContent, Box, DialogActions, Button, TextField } from '@mui/material'
+import { IconButton, Card, CardActions, CardContent, Stack, Grid, Dialog, DialogTitle, DialogContent, Box, DialogActions, Button, TextField, IconButtonProps, CardHeader, Avatar, Collapse } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import ShareIcon from '@mui/icons-material/Share'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import FullscreenIcon from '@mui/icons-material/Fullscreen'
 import { useRef, useState } from 'react'
 import { Typography } from '@mui/material'
 import Tune from './tune'
 import { QRCodeSVG } from 'qrcode.react'
 import { objectSerDeser } from '../url'
+import { styled } from '@mui/material/styles';
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: 'rotate(0deg)',
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: 'rotate(180deg)',
+      },
+    },
+  ],
+}));
 
 export default function LightComponent({ index, currentTimestamp, light, lightConfig, onLightSettingsChange, onDelete, style }: { index: number, currentTimestamp: number, light: TrafficLight, lightConfig: LightConfig, onLightSettingsChange: (lightSettings: LightSettings) => void, onDelete?: () => void, style?: React.CSSProperties }) {
 
@@ -21,11 +51,17 @@ export default function LightComponent({ index, currentTimestamp, light, lightCo
 
   const [shareMode, setShareMode] = useState<Boolean>(false)
 
+  const [expanded, setExpanded] = useState(true)
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   const deleteButton = onDelete == null ? <></> : <IconButton aria-label="delete" onClick={() => onDelete()}><DeleteIcon /></IconButton>
 
   const currentPhase = light.currentPhase(currentTimestamp)
 
-  const lightImg = <img className="the-traffic-light" ref={lightRef} src={currentPhase.state.file} alt={currentPhase.state.name} style={{ width: "200px", maxWidth: "100%", maxHeight: "90vh" }} />
+  const lightImg = <img className="the-traffic-light" ref={lightRef} src={currentPhase.state.file} alt={currentPhase.state.name} style={{ maxWidth: "100%", maxHeight: "70vh" }} />
   const tune = <Tune lightConfig={lightConfig} onLightSettingsChange={onLightSettingsChange} />
 
   const search = `?crossingSettings=${objectSerDeser().serialize(lightConfig.crossingSettings)}&lightSettings=${objectSerDeser().serialize([lightConfig.toLightSettings()])}`
@@ -73,26 +109,46 @@ export default function LightComponent({ index, currentTimestamp, light, lightCo
 
   return (
     <Card style={style}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14, mb: 1.5 }} color="text.secondary" gutterBottom>
-          Light #{index} {currentTimestamp}
-        </Typography>
-        <Grid container sx={{ justifyContent: "space-between" }}>
-          <Grid item xs={8}>
-            <Stack direction="column" alignItems="stretch">
-              <form>
-                {durationInputs}
-                <Input label="Offset" id={`light-${index}-offset`} min={0} max={(lightConfig.cycleLength() / 1000) - 1} value={lightConfig.offset / 1000} onChange={e => onLightSettingsChange(lightConfig.withOffset(e.target.value * 1000))} />
-              </form>
-            </Stack>
-          </Grid>
-          <Grid item xs={4}>
-            <Grid container sx={{ alignItems: "center", justifyContent: "center" }}>
-              {lightImg}
+      <CardHeader
+        avatar={
+          <Avatar 
+            aria-label="traffic-light" 
+            sx={{ bgcolor: currentPhase.state.color }}
+          >
+            {index}
+          </Avatar>
+        }
+        action={
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+        </ExpandMore>
+        }
+        title={`Traffic Light #${index}`}
+      />
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Grid container sx={{ justifyContent: "space-between", alignItems: "center" }} spacing={4}>
+            <Grid item xs={8} md={10}>
+              <Stack direction="column" alignItems="stretch">
+                <form>
+                  {durationInputs}
+                  <Input label="Offset" id={`light-${index}-offset`} min={0} max={(lightConfig.cycleLength() / 1000) - 1} value={lightConfig.offset / 1000} onChange={e => onLightSettingsChange(lightConfig.withOffset(e.target.value * 1000))} />
+                </form>
+              </Stack>
+            </Grid>
+            <Grid item xs={4} md={2}>
+              <Grid container sx={{ alignItems: "center", justifyContent: "center" }}>
+                {lightImg}
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
+        </CardContent>
+      </Collapse>
       <CardActions>
         <IconButton aria-label="fullscreen" onClick={() => enterFullScreen()}><FullscreenIcon /></IconButton>
         <IconButton aria-label="share" onClick={() => setShareMode(!shareMode) }><ShareIcon /></IconButton>
