f3c2f8ebb86a24d957500c07f19f3aef68a89447
Mateusz <mateusz@adhese.eu>
2024-12-20 23:33:36 +0100

    Cleanup

--- app/components/IntersectionSettingsPanel.tsx
+++ app/components/IntersectionSettingsPanel.tsx
@@ -8,30 +8,6 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import React from 'react'
 import GridGoldenratioIcon from '@mui/icons-material/GridGoldenratio'
 
-interface TabPanelProps {
-  children?: React.ReactNode
-  index: number
-  value: number | false
-}
-
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <Collapse in={value == index} unmountOnExit>
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && <Box>{children}</Box>}
-      </div>
-    </Collapse>
-  )
-}
-
 export default function IntersectionSettingsPanel({ 
   intersectionSettings, 
   updateIntersectionSettings,
@@ -48,15 +24,11 @@ export default function IntersectionSettingsPanel({
 
   const [selectedTab, setSelectedTab] = useState<number | false>(false)
 
-  const handleTabChange = (newValue: number) => {
-    setSelectedTab(newValue === selectedTab ? false : newValue)
-  }
-
   function a11yProps(index: number) {
     return {
       id: `simple-tab-${index}`,
       'aria-controls': `simple-tabpanel-${index}`,
-      onClick: () => handleTabChange(index),
+      onClick: () => setSelectedTab(index === selectedTab ? false : index),
     }
   }
 
@@ -67,7 +39,7 @@ export default function IntersectionSettingsPanel({
         <Tab icon={<AccessTimeIcon />} label='Time' iconPosition='top' {...a11yProps(1)} />
       </Tabs>
 
-      <CustomTabPanel value={selectedTab} index={0}>
+      <Collapse in={selectedTab === 0} unmountOnExit>
         <CardContent>
           <Input 
             label="Cycle length (s)"
@@ -95,8 +67,8 @@ export default function IntersectionSettingsPanel({
             onChange={ e => updateIntersectionSettings({ ...intersectionSettings, failure: { duration: intersectionSettings.failure.duration, probability: Number(e.target.value) / 100 } }) } 
           />
         </CardContent>
-      </CustomTabPanel>
-      <CustomTabPanel value={selectedTab} index={1}>
+      </Collapse>
+      <Collapse in={selectedTab === 1} unmountOnExit>
         <CardContent>
           <Input 
             label="Time correction (s)" 
@@ -109,7 +81,7 @@ export default function IntersectionSettingsPanel({
           />
           <Button variant='outlined' onClick={initTimeSync}>Sync time</Button>
         </CardContent>
-      </CustomTabPanel>
+      </Collapse>
     </Card>
   )
 }
