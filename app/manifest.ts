ca59ba060157c426f952ba77df06329b2d076976
Mateusz <mateusz@adhese.eu>
2025-02-24 23:30:18 +0100

    Undo mainfest changes

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#f44336",
+    theme_color: "#000000",
     icons: [
       {
         src: "/icon.svg",
